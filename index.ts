9c5edadd40590c47bce6bd1dbf5095e7c88f1bd3
vivekcsein <vivekcse.in@gmail.com>
2026-06-28 19:06:49 +0530

    handle function changed

--- index.ts
+++ index.ts
@@ -1,13 +1,4 @@
-import type { Hono } from 'hono';
 import { handle } from 'hono/vercel';
-import createApp from "./src/app/app";
-import type { BlankEnv, BlankSchema } from 'hono/types';
+import createApp from './src/app/app';
 
-const app = createApp();
-const handler = handle(app as unknown as Hono<BlankEnv, BlankSchema, "/">);
-
-export const GET = handler;
-export const POST = handler;
-export const PATCH = handler;
-export const PUT = handler;
-export const OPTIONS = handler;
+export default handle(await createApp());
