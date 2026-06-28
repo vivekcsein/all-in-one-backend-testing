a6d59fe7459f6c570345dea439a619f67c972f11
vivekcsein <vivekcse.in@gmail.com>
2026-06-28 19:28:31 +0530

    more flexible index.ts created

--- index.ts
+++ index.ts
@@ -1,9 +1,17 @@
 import createApp from '@/app/app';
 import { handle } from 'hono/vercel';
 
+let appPromise: ReturnType<typeof createApp> | null = null;
+
+const getApp = async () => {
+  if (!appPromise) {
+    appPromise = createApp();
+  }
+  return await appPromise;
+};
+
 const handler = async (req: Request) => {
-  // Wait for the app to be created
-  const app = await createApp();
+  const app = await getApp();
   return handle(app)(req);
 };
 
@@ -13,8 +21,8 @@ export const PATCH = handler;
 export const PUT = handler;
 export const OPTIONS = handler;
 
-// Or, if you want the simpler all-in-one:
+// Or, simpler all-in-one:
 export default async function appHandler(req: Request) {
-  const app = await createApp();
+  const app = await getApp();
   return handle(app)(req);
 }
