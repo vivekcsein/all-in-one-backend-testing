abc0ebc44ebe8ce19e54b72a002abe38cc0ec25c
vivekcsein <vivekcse.in@gmail.com>
2026-06-28 19:03:18 +0530

    typecast from unknown to good

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import type { Hono } from 'hono';
 import { handle } from 'hono/vercel';
 import createApp from "./src/app/app";
+import type { BlankEnv, BlankSchema } from 'hono/types';
 
 const app = createApp();
-const handler = handle(app as unknown as Hono);
+const handler = handle(app as unknown as Hono<BlankEnv, BlankSchema, "/">);
+
 export const GET = handler;
 export const POST = handler;
 export const PATCH = handler;
