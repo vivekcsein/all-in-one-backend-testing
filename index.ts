38c5ef7ade109edcd2be5eb09e3aaede8be3f349
vivekcsein <vivekcse.in@gmail.com>
2026-06-28 19:20:03 +0530

    promised resolved

--- index.ts
+++ index.ts
@@ -1,12 +1,20 @@
 import createApp from '@/app/app';
 import { handle } from 'hono/vercel';
 
-const app = createApp();
-
-const handler = handle(app);
+const handler = async (req: Request) => {
+  // Wait for the app to be created
+  const app = await createApp();
+  return handle(app)(req);
+};
 
 export const GET = handler;
 export const POST = handler;
 export const PATCH = handler;
 export const PUT = handler;
 export const OPTIONS = handler;
+
+// Or, if you want the simpler all-in-one:
+export default async function appHandler(req: Request) {
+  const app = await createApp();
+  return handle(app)(req);
+}
